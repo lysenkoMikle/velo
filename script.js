5b0784fa44e3badddcc2590da60afa3026d58a2e
lysenkoMikle <miklee95@gmail.com>
2022-06-16 19:56:18 +0300

    update

--- script.js
+++ script.js
@@ -37,3 +37,32 @@ menuItem.forEach((item) => {
 		});
 	});
 });
+
+let popupBg = document.querySelector(".popup_bg");
+let popup = document.querySelector(".popup");
+let openPopupButtons = document.querySelectorAll(".open_popup");
+let closePopupButton = document.querySelector(".close_popup");
+const body = document.querySelector("body");
+
+openPopupButtons.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		e.preventDefault();
+		document.body.classList.add("notOverflow");
+		popupBg.classList.add("activePop");
+		popup.classList.add("activePop");
+	});
+});
+
+closePopupButton.addEventListener("click", () => {
+	popupBg.classList.remove("activePop");
+	popup.classList.remove("activePop");
+	document.body.classList.remove("notOverflow");
+});
+
+document.addEventListener("click", (e) => {
+	if (e.target === popupBg) {
+		popupBg.classList.remove("activePop");
+		popup.classList.remove("activePop");
+		document.body.classList.remove("notOverflow");
+	}
+});
